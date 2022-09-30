8751662869abbd4d0ab70d643488ec2ad74b6fa5
JULIAN MAZO <82735837+BERTOTEM@users.noreply.github.com>
2022-09-30 01:29:56 -0500

    login

---
+++ EmpresaTec.App/EmpresaTec.App.Sitioweb/Pages/Inicio.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmpresaTec.App.Sitioweb.Pages;
+
+public class InicioModel : PageModel
+{
+    private readonly ILogger<InicioModel> _logger;
+
+    public InicioModel(ILogger<InicioModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
